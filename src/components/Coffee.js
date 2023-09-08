81a371d7829a7c479d7a63b72837303efb14dafb
Lindsay Warr <iamalion@gmail.com>
2023-09-08 09:36:26 -0700

    Add proptypes

--- src/components/Coffee.js
+++ src/components/Coffee.js
@@ -7,15 +7,31 @@ function Coffee(props){
     <React.Fragment>
       <div onClick = {() => props.whenCoffeeClicked(props.id)}>
         <h3>{props.name} - <em>{props.origin}</em></h3>
-        <p><em></em></p>
-        <hr/>
+        <p>{description}</p>
+        <p><em>{props.roast} roast</em></p>
+        <p><em>{props.price} per pound</em></p>
+        <p><em>{props.available} pounds available</em></p>
+        <button onClick={() => props.whenBuyClicked(props.id)}>Buy</button>
+        <button onClick={() => props.whenEditClicked(props.id)}>Update</button>
+        <button onClick={() => props.whenDeleteClicked(props.id)}>Delete</button>
+      <hr/>
       </div>
     </React.Fragment>
   );
 }
 
 Coffee.propTypes = {
-    n
+  name: PropTypes.string,
+  origin: PropTypes.string,
+  description: PropTypes.string,
+  roast: PropTypes.string,
+  price: PropTypes.number,
+  available: PropTypes.number,
+  id: PropTypes.string,
+  whenCoffeeClicked: PropTypes.func,
+  whenBuyClicked: PropTypes.func,
+  whenEditClicked: PropTypes.func,
+  whenDeleteClicked: PropTypes.func
   };
   
 
