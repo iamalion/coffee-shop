2312037d68551c1a81cb85793a1411777cdb4d29
Lindsay Warr <iamalion@gmail.com>
2023-09-09 08:41:00 -0700

    Add junk file for unused code

---
+++ src/components/xjunk.js
@@ -0,0 +1,14 @@
+// Unused code:
+//   handleClick = () => {
+//     if (this.state.selectedCoffee != null) {
+//       this.setState({
+//         formVisibleOnPage: false,
+//         selectedCoffee: null,
+//         editing: false
+//       });
+//     } else {
+//       this.setState(prevState => ({
+//         formVisibleOnPage: !prevState.formVisibleOnPage,
+//       }));
+//     }
+//   }
