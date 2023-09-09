8cffc7e89b5ac3ed3d7b1812f1c6160b85eb75aa
Lindsay Warr <iamalion@gmail.com>
2023-09-09 08:38:30 -0700

    Remove unused code from CoffeeControl

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Header from './Header';
 import CoffeeControl from './CoffeeControl';
 
 const App = () => {
   return (
     <div className='container'>
-      {/* <Header /> */}
       <CoffeeControl />
     </div>
   );
--- src/components/CoffeeControl.js
+++ src/components/CoffeeControl.js
@@ -64,19 +64,7 @@ class CoffeeControl extends React.Component {
     };
   }
 
-  handleClick = () => {
-    if (this.state.selectedCoffee != null) {
-      this.setState({
-        formVisibleOnPage: false,
-        selectedCoffee: null,
-        editing: false
-      });
-    } else {
-      this.setState(prevState => ({
-        formVisibleOnPage: !prevState.formVisibleOnPage,
-      }));
-    }
-  }
+
   handleAddButtonClick = () => {
     this.setState((prevState) => ({
       formVisibleOnPage: !prevState.formVisibleOnPage,
